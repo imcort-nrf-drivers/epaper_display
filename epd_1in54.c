b04d30f4bb8456071f3af418eefbdf1350aaa0c9
imcort <179376503@qq.com>
2023-02-03 15:01:15 +0800

    ok

--- epd_1in54.c
+++ epd_1in54.c
@@ -1,7 +1,6 @@
 #include "epd_1in54.h"
 
-
-static uint8_t cmdBuf[4];
+static uint8_t cmdBuf[10];
 static uint8_t epd_framebuffer[SCREEN_WIDTH * SCREEN_HEIGHT / 8];
 
 static const uint8_t set[] = {1, 2, 4, 8, 16, 32, 64, 128},
@@ -25,7 +24,7 @@ void epd_writeCommand(uint8_t cmd, uint8_t* data, int32_t len)
     digitalWrite(EINK_CS, LOW);
     digitalWrite(EINK_DC, LOW);
     
-	spi_transfer(&cmd, 1, &cmd, 0);
+	spi_transfer(&cmd, 1, NULL, 0);
     
     if(len)
     {
@@ -42,7 +41,7 @@ void epd_begin(void)
     pinMode(EINK_CS, OUTPUT);
     pinMode(EINK_DC, OUTPUT);
     pinMode(EINK_RESET, OUTPUT);
-    pinMode(EINK_BUSY, INPUT_PULLUP);
+    pinMode(EINK_BUSY, INPUT);
     
     digitalWrite(EINK_CS, HIGH);
     
@@ -53,84 +52,52 @@ void epd_begin(void)
     digitalWrite(EINK_RESET, HIGH);
     delay(10);
     
-	epd_waitBusy();
 	epd_writeCommand(0x12, NULL, 0);  //SWRESET
     delay(10);
-	epd_waitBusy();
     
     cmdBuf[0] = 0xC7;
     cmdBuf[1] = 0x00;
     cmdBuf[2] = 0x00;
 	epd_writeCommand(0x01, cmdBuf, 3); //Driver output control
 
-    cmdBuf[0] = 0x01;
-	epd_writeCommand(0x11, cmdBuf, 1); //data entry mode
-    
-    cmdBuf[0] = 0x00;
-    cmdBuf[1] = 0x18;
-	epd_writeCommand(0x44, cmdBuf, 2); //set Ram-X address start/end position 200  
-
-    cmdBuf[0] = 0xC7;
-    cmdBuf[1] = 0x00;
-    cmdBuf[2] = 0x00;
-    cmdBuf[3] = 0x00;
-	epd_writeCommand(0x45, cmdBuf, 4); //set Ram-Y address start/end position 200
-    
     cmdBuf[0] = 0x05;
 	epd_writeCommand(0x3C, cmdBuf, 1); //Border Wavefrom
     
     cmdBuf[0] = 0x80;
 	epd_writeCommand(0x18, cmdBuf, 1); //Internal temperature
-    
-    cmdBuf[0] = 0xf8;
-	epd_writeCommand(0x22, cmdBuf, 1); //Load LUT
-    
-    epd_writeCommand(0x20, NULL, 0);  //Master Activation
-    
-    epd_waitBusy();
-    
-    cmdBuf[0] = 0x00;
-	epd_writeCommand(0x4E, cmdBuf, 1); //set RAM x address count to 0;
-    
-    cmdBuf[0] = 0xC7;
-    cmdBuf[1] = 0x00;
-	epd_writeCommand(0x4F, cmdBuf, 2); //set RAM y address count to 0X199;
-    
-    epd_waitBusy();
 	
 }
 
-void epd_setMemoryArea(int x_start, int y_start, int x_end, int y_end)
+void epd_setPartialRamArea(uint16_t x, uint16_t y, uint16_t w, uint16_t h)
 {
     
-    cmdBuf[0] = x_start & 0xFF;
-    cmdBuf[1] = x_end & 0xFF;
-	epd_writeCommand(0x44, cmdBuf, 2);
-    
-    cmdBuf[0] = y_start & 0xFF;
-    cmdBuf[1] = (y_start >> 8) & 0xFF;
-    cmdBuf[2] = y_end & 0xFF;
-    cmdBuf[3] = (y_end >> 8) & 0xFF;
+    cmdBuf[0] = 0x03;
+    epd_writeCommand(0x11, cmdBuf, 1); //data entry mode
+
+    cmdBuf[0] = x / 8;
+    cmdBuf[1] = (x + w - 1) / 8;
+    epd_writeCommand(0x44, cmdBuf, 2);
+
+    cmdBuf[0] = y % 256;
+    cmdBuf[1] = y / 256;
+    cmdBuf[2] = (y + h - 1) % 256;
+    cmdBuf[3] = (y + h - 1) / 256;
     epd_writeCommand(0x45, cmdBuf, 4);
     
-}
-
-void epd_setMemoryPointer(int x, int y)
-{
-    
-    /* x point must be the multiple of 8 or the last 3 bits will be ignored */
-    cmdBuf[0] = (x >> 3) & 0xFF;
+    cmdBuf[0] = x / 8;
 	epd_writeCommand(0x4E, cmdBuf, 1);
     
-    cmdBuf[0] = y & 0xFF;
-    cmdBuf[1] = (y >> 8) & 0xFF;
+    cmdBuf[0] = y % 256;
+    cmdBuf[1] = y / 256;
 	epd_writeCommand(0x4F, cmdBuf, 2);
     
-    //epd_waitBusy();
+    epd_waitBusy();
 }
 
 void epd_clearScreen(void)
 {
+    
+    epd_setPartialRamArea(0, 0, 200, 200);
     
     cmdBuf[0] = 0x24;
     
@@ -156,24 +123,8 @@ void epd_clearScreen(void)
 
 void epd_sendFrame(int x, int y, int w, int h, uint8_t* frame_buffer)
 {
-    epd_setMemoryArea(x / 8, y - 1, x / 8 + w / 8 - 1, y + h - 1);
-    epd_setMemoryPointer(x, y);
     
-//    cmdBuf[0] = 0x24;
-//    
-//    digitalWrite(EINK_DC, LOW);
-//    digitalWrite(EINK_CS, LOW);
-//    
-//    spi_transfer(cmdBuf, 1, NULL, 0);
-//    
-//    digitalWrite(EINK_DC, HIGH);
-//    
-//    for(int32_t i = 0; i < (w * h / 8); i++)
-//    {
-//        spi_transfer(frame_buffer + i, 1, NULL, 0);
-//    }
-//    
-//    digitalWrite(EINK_CS, HIGH);
+    epd_setPartialRamArea(x, y, w, h);
     
     epd_writeCommand(0x24, frame_buffer, w * h / 8);
 }
@@ -201,25 +152,6 @@ void epd_update_u8(uint8_t y, uint8_t* line_buffer)
 
 void epd_update_framebuffer(void)
 {
-    
-    cmdBuf[0] = 0x00;
-    cmdBuf[1] = 0x18;
-	epd_writeCommand(0x44, cmdBuf, 2); //set Ram-X address start/end position 200  
-
-    cmdBuf[0] = 0xC7;
-    cmdBuf[1] = 0x00;
-    cmdBuf[2] = 0x00;
-    cmdBuf[3] = 0x00;
-	epd_writeCommand(0x45, cmdBuf, 4); //set Ram-Y address start/end position 200
-    
-    cmdBuf[0] = 0x00;
-	epd_writeCommand(0x4E, cmdBuf, 1); //set RAM x address count to 0;
-    
-    cmdBuf[0] = 0xC7;
-    cmdBuf[1] = 0x00;
-	epd_writeCommand(0x4F, cmdBuf, 2); //set RAM y address count to 0X199;
-    
-    epd_waitBusy();
     
     cmdBuf[0] = 0x24;
     
--- epd_1in54.h
+++ epd_1in54.h
@@ -7,8 +7,6 @@
 #define SCREEN_HEIGHT 200
 
 void epd_begin(void);
-void epd_setMemoryArea(int x_start, int y_start, int x_end, int y_end);
-void epd_setMemoryPointer(int x, int y);
 void epd_sendFrame(int x, int y, int w, int h, uint8_t* frame_buffer);
 void epd_displayFrame(void);
 void epd_displayPartFrame(void);
@@ -16,6 +14,7 @@ void epd_lowPower(void);
 void epd_clearScreen(void);
 void epd_update_u8(uint8_t y, uint8_t* line_buffer);
 void epd_update_framebuffer(void);
+void epd_setPartialRamArea(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
 
 #endif
 
